a316ac0587374a6264415c5c4bdf9c5afbd7a5b1
Jaybie <neli.mashele@gmail.com>
2025-03-03 17:09:33 +0200

    Add project files.

---
+++ jayStudyGroup/Controllers/groupController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jayStudyGroup.Controllers
+{
+    public class groupController : Controller
+    {
+        // GET: group
+        public ActionResult Index()
+        {
+            List<Models.memberModel> members = new List<Models.memberModel>();
+
+            members.Add(new Models.memberModel { stuNumber = "u", fName = "Raelene", lName = "Dokkoo", email = "raelene.d@gmail.com" });
+            members.Add(new Models.memberModel { stuNumber = "u23854830", fName = "Dakalo", lName = "Nemamilwe", email = "u23854830@tuks.co.za" });
+            members.Add(new Models.memberModel { stuNumber = "Name 3", fName = "Boikgantsho", lName = "More", email = "true" });
+            members.Add(new Models.memberModel { stuNumber = "Name 4", fName = "Surname 4", lName = "4", email = "false" });
+            members.Add(new Models.memberModel { stuNumber = "Name 5", fName = "Raelene", lName = "Dokkoo", email = "true" });
+
+            return View(members);
+        }
+    }
+}
---
+++ jayStudyGroup/Models/memberModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace jayStudyGroup.Models
+{
+	public class memberModel
+	{
+        [Display(Name = "STUDENT NUMBER")]
+        public string stuNumber { get; set; }
+
+        [Display(Name = "NAME")]
+        public string fName { get; set; }
+
+        [Display(Name = "SURNAME")]
+        public string lName { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")]
+        public string email { get; set; }
+    }
+}
